7b1bd56fbb3b65c21ab91d9ed79209682ecdfb23
Toni <tonheino@gmail.com>
2023-11-16 01:54:32 +0200

    Implemented Register Page with Firebase Integration
    Added functionality to the Register page enabling user registration by leveraging Firebase authentication.
    
    Enabled user registration through email and password, with additional fields for full name and profile picture upload.
    Integrated Firebase functionalities for creating user accounts, updating user profile information, and handling potential registration errors.

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,43 +1,71 @@
 import React, { useState } from 'react';
-import { IonContent, IonInput, IonButton } from '@ionic/react';
-import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+import { IonInput, IonButton, IonBackButton } from '@ionic/react';
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage'; // Adjusted import for Firebase Storage
 import './styles/register.css';
 
-const Register: React.FC = () => {
+  const Register: React.FC = () => {
+
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [fullName, setFullName] = useState('');
+  const [profilePicture, setProfilePicture] = useState<File | null>(null);
   const [error, setError] = useState<string | null>(null);
 
   const handleRegister = async () => {
     try {
       const auth = getAuth();
       const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-      // If registration is successful, you can handle the userCredential or redirect the user
+
+      if (profilePicture) {
+        const storage = getStorage(); // Get the storage instance
+        const storageRef = ref(storage, `profilePictures/${userCredential.user.uid}`);
+        await uploadBytes(storageRef, profilePicture);
+
+        // Update user profile information
+        await updateProfile(userCredential.user, {
+          displayName: fullName,
+          photoURL: await getDownloadURL(storageRef),
+        });
+      } else {
+        // Update user profile information (without profile picture)
+        await updateProfile(userCredential.user, {
+          displayName: fullName,
+        });
+      }
+
       console.log('User registered:', userCredential.user);
     } catch (err: any) {
       setError(err.message || 'An error occurred during registration.');
     }
   };
 
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files[0]) {
+      setProfilePicture(event.target.files[0]);
+    }
+  };
+
   return (
-    <IonContent>
-      <div>
-        <IonInput
-          type="email"
-          placeholder="Email"
-          value={email}
-          onIonChange={(e) => setEmail(e.detail.value!)}
-        />
-        <IonInput
-          type="password"
-          placeholder="Password"
-          value={password}
-          onIonChange={(e) => setPassword(e.detail.value!)}
-        />
-        <IonButton onClick={handleRegister}>Register</IonButton>
+    <div className='tausta'>
+      <div className='sisalto'>
+        <IonBackButton className='takaisin' defaultHref="/logreg" />
+        <div className="registerTxt">Register</div>
+        <img className="registerKuva" src="./src/images/register.png" />
+        
+        <IonInput className='input' type="email" placeholder="Email" value={email} onIonChange={(e) => setEmail(e.detail.value!)}/>
+        <IonInput className='input' type="password" placeholder="Password" value={password} onIonChange={(e) => setPassword(e.detail.value!)}/>
+        <IonInput className='input' type="text" placeholder="Full Name" value={fullName} onIonChange={(e) => setFullName(e.detail.value!)}/>
+       
+        <label htmlFor="tiedostoLataus" className="fileInputLabel">
+          Upload Profile Picture
+        </label>
+        <input id='tiedostoLataus' type="file" accept="image/*" onChange={handleFileChange} />
+      
+        <IonButton className='registerNappi' onClick={handleRegister}>Register</IonButton>
         {error && <p>{error}</p>}
       </div>
-    </IonContent>
+    </div>
   );
 };
 
